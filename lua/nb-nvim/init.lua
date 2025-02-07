d2150bc3477f4c8149ed0f4618d29a8d3ead7789
shoebox <shoeboks@gmail.com>
2025-02-07 10:26:15 +0000

    fix(init): updates init

--- lua/nb-nvim/init.lua
+++ lua/nb-nvim/init.lua
@@ -1,25 +1,24 @@
 local client = require("nb-nvim.client")
-local journal = require("nb-nvim.journal")
-journal.setup(client)
-local picker = require("nb-nvim.picker")
-
--- local notebooks = require("nb-nvim.notebooks")
 local commands = require("nb-nvim.commands")
+local journal = require("nb-nvim.journal")
 local nb = setmetatable({}, { __index = journal })
 
 nb.__index = function(table, key)
-	if journal[key] then
-		return journal[key].setup(client)
-	elseif commands[key] then
-		return commands[key]
-	else
-		return nil -- or default behavior
-	end
+  if journal[key] then
+    return journal[key]
+  elseif commands[key] then
+    return commands[key]
+  else
+    return nil -- or default behavior
+  end
 end
 
 function nb.setup(opts)
-	opts = opts or {}
-	commands.setup(client)
+  opts = opts or {}
+  client.executor.debug = opts.debug
+
+  commands.setup(client)
+  journal.setup(client)
 end
 
 setmetatable(nb, nb) -- set M as its own metatable for indexing
