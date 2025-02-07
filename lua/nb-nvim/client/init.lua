daef39bc24e5d56fa643a0b8162becb13a710a38
shoebox <shoeboks@gmail.com>
2025-02-07 10:25:12 +0000

    fix(client): updates initializaion

--- lua/nb-nvim/client/init.lua
+++ lua/nb-nvim/client/init.lua
@@ -1,8 +1,8 @@
 local M = {
-	exec = require("nb-nvim.client.execute"),
-	note = require("nb-nvim.client.note"),
-	notebook = require("nb-nvim.client.notebook"),
-	config = require("nb-nvim.config"),
+  executor = require("nb-nvim.client.executor"),
+  note = require("nb-nvim.client.note"),
+  notebook = require("nb-nvim.client.notebook"),
+  config = require("nb-nvim.config"),
 }
 
 return M
